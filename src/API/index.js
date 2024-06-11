3066bc790682a668f5d7f7ea2ba2e3b0aaf37e80
KhanhRomVN <khanhromvn@gmail.com>
2024-06-11 18:11:48 +0700

    update

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,5 @@
-// export const BACKEND_URI = 'https://shopping-mern-stack-backend.onrender.com'
-export const BACKEND_URI = 'http://localhost:8080'
+export const BACKEND_URI = 'https://shopping-mern-stack-backend.onrender.com'
+// export const BACKEND_URI = 'http://localhost:8080'
 
 export const loginAPI = `${BACKEND_URI}/auth/login`
 export const registerAPI = `${BACKEND_URI}/auth/register`
